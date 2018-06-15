1a7db690dc15344515d433ba3b2b9ac69bb638b7
stan wielga <svwielga4@gmail.com>
2018-06-15 16:55:23 -0700

    early return examples and happy paths

--- index.php
+++ index.php
@@ -1,6 +1,19 @@
 <?php
 
-function signUp($subscription)
+function signUp(Subscription $subscription)
 {
-
+    $subscription->create();
 }
+
+function getSubscriptionType($type)
+{
+    if ($type == 'forever') {
+        return new ForeverSubscription();
+    }
+
+    return new MonthlySubscription();
+}
+
+$subscription = getSubscriptionType($type);
+
+signUp($subscription);
