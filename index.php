da4b3ccfee50dda51076820b92ab23cc52c0d29e
stan wielga <svwielga4@gmail.com>
2018-06-15 20:20:00 -0700

    practicing one level indents

--- index.php
+++ index.php
@@ -1,19 +1,67 @@
 <?php
 
-function signUp(Subscription $subscription)
+class BankAccounts
 {
-    $subscription->create();
-}
+    protected $accounts;
 
-function getSubscriptionType($type)
-{
-    if ($type == 'forever') {
-        return new ForeverSubscription();
+    public function __construct($accounts)
+    {
+        $this->accounts = $accounts;
     }
 
-    return new MonthlySubscription();
+    public function filterBy($accountType)
+    {
+
+        return array_filter($this->accounts, function ($account) use ($accountType) {
+
+            return $account->isOfType($accountType);
+
+        });
+
+    }
+
 }
 
-$subscription = getSubscriptionType($type);
+class Account
+{
 
-signUp($subscription);
+    protected $type;
+
+    private function __construct($type)
+    {
+        $this->type = $type;
+    }
+
+    public static function open($type)
+    {
+        return new static($type);
+    }
+
+    public function isOfType($accountType)
+    {
+        return $this->type() == $accountType && $this->isActive();
+    }
+    private function type()
+    {
+        return $this->type;
+    }
+
+    private function isActive()
+    {
+        return true;
+    }
+
+}
+
+$accounts = [
+    Account::open('checking'),
+    Account::open('savings'),
+    Account::open('checking'),
+    Account::open('savings'),
+];
+
+$accounts = new BankAccounts($accounts);
+
+$savings = $accounts->filterBy('savings');
+
+var_dump($savings);
